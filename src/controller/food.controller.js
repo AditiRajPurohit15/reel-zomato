0898854082a495959678e7a6cf9c876bec3ea907
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-02-24 22:56:00 +0530

    like food api

--- src/controller/food.controller.js
+++ src/controller/food.controller.js
@@ -1,6 +1,7 @@
 const foodPartnerModel = require('../model/foodPartner.model')
 const userModel = require('../model/auth.model')
 const foodModel = require('../model/food.model');
+const likeModel = require('../model/likes.model')
 const {uploadFile} = require('../services/storage.service')
 const { v4: uuid } = require("uuid")
 
@@ -39,6 +40,46 @@ async function getFoodItem(req,res){
         res.status(400).json({
             message: "error in fetching food items",
             error: error
+        })
+    }
+}
+
+async function likeFood(req,res){
+    try {
+        const {foodId} = req.body;
+        const user= req.user;
+
+        const isAlreadyLiked = await likeModel.findOne({
+            user: user._id,
+            food: foodId
+        })
+        if(isAlreadyLiked){
+            await likeModel.deleteOne({
+                user:user._id,
+                food: foodId
+            })
+            await foodModel.findByIdAndUpdate(foodId,{
+                $inc: {likeCount: -1}
+            })
+            return res.status(200).json({
+                message: "food unliked successfully"
+            })
+        }
+        const like = await likeModel.create({
+            user: user._id,
+            food: foodId
+        })
+        await foodModel.findByIdAndUpdate(foodId,{
+            $inc : {likeCount: 1}
+        })
+        return res.status(201).json({
+            message: "food liked successfully",
+            like
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "error in likeFood",
+            error: error.message
         })
     }
 }
