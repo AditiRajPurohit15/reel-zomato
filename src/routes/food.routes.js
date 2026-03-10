b5171ad6eabb6d12ed3f4f2972bf22554d09d65f
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-03-10 16:17:58 +0530

    save route created

--- src/routes/food.routes.js
+++ src/routes/food.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {authFoodPartnerMiddleware, authUserMiddleware} = require('../middleware/auth.middleware')
-const {createFood, getFoodItem, likeFood} = require('../controller/food.controller')
+const {createFood, getFoodItem, likeFood, saveFood} = require('../controller/food.controller')
 const router = express.Router();
 const multer = require ('multer');
 
@@ -12,5 +12,6 @@ const upload = multer({
 router.post('/',authFoodPartnerMiddleware,upload.single("video"),createFood)
 router.get('/', authUserMiddleware, getFoodItem)
 router.post('/like',authUserMiddleware ,likeFood)
+router.post('/save',authUserMiddleware,saveFood)
 
 module.exports = router;
