2df6114761b4006986536a496819fad4245550bf
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-03-16 15:31:43 +0530

    get food feed for partner route done

--- src/routes/food.routes.js
+++ src/routes/food.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {authFoodPartnerMiddleware, authUserMiddleware} = require('../middleware/auth.middleware')
-const {createFood, getFoodItem, likeFood, saveFood} = require('../controller/food.controller')
+const {createFood, getFoodItem, likeFood, saveFood, getFoodForPartner} = require('../controller/food.controller')
 const router = express.Router();
 const multer = require ('multer');
 
@@ -13,5 +13,6 @@ router.post('/',authFoodPartnerMiddleware,upload.single("video"),createFood)
 router.get('/', authUserMiddleware, getFoodItem)
 router.post('/like',authUserMiddleware ,likeFood)
 router.post('/save',authUserMiddleware,saveFood)
+router.get('/getFood',authFoodPartnerMiddleware,getFoodForPartner);
 
 module.exports = router;
