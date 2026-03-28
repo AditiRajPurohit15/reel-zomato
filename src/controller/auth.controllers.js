be74c6528370d649bd4b2b9882c6bba480788993
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-03-28 22:35:06 +0530

    cookie

--- src/controller/auth.controllers.js
+++ src/controller/auth.controllers.js
@@ -60,7 +60,11 @@ const loginController = async(req,res)=>{
             })
         }
         let token = user.generateToken();
-        res.cookie("token",token);
+        res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,        // REQUIRED on production (HTTPS)
+        sameSite: "None",    // VERY IMPORTANT for cross-origin
+        });
 
         const userObj = user.toObject();
         delete userObj.password;
