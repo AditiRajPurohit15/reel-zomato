090d4c4392c9b573ad70c78abca93e415d1f7840
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-05-30 16:05:01 +0530

    comment field

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const foodRoutes = require('./routes/food.routes');
 const meRoute = require('./routes/me.routes');
+const commentRoutes = require('./routes/comment.routes');
 
 const app = express();
 const allowedOrigins = [
@@ -28,6 +29,7 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/food',foodRoutes);
 app.use('/api/profile',meRoute);
+app.use('/api/comments',commentRoutes);
 
 
 module.exports = app;
