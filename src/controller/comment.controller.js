6055d95b6f3bd9c6bb2e19c35aa2b6b653e62639
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-05-31 18:01:26 +0530

    feat: add comments CRUD APIs

--- src/controller/comment.controller.js
+++ src/controller/comment.controller.js
@@ -63,7 +63,29 @@ try {
 }
 
 async function deleteComment(req,res){
-
+try {
+    const comment = await commentModel.findById(req.params.commentId);
+    if(!comment){
+        return res.status(404).json({
+            message:"comment not found"
+        })
+    }
+    if(comment.user.toString()!==req.user._id.toString()){
+        return res.status(403).json({
+            message:"you are not authorized to delete this comment!"
+        })
+    }
+    await commentModel.findByIdAndDelete(req.params.commentId);
+    return res.status(200).json({
+        message:"comment deleted successfully",
+    })
+    
+} catch (error) {
+    return res.status(500).json({
+            message: "error in deleting comment",
+            error:error.message
+        }) 
+}
 }
 
 module.exports ={
