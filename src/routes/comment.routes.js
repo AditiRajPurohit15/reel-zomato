8926604d9c244713d7a4fb5997d7472af9211e79
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-06-02 12:54:49 +0530

    getReplies route

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -4,7 +4,8 @@ const {
   postComment,
   getComment,
   deleteComment,
-  replyToComment
+  replyToComment,
+  getReplies
 } = require('../controller/comment.controller');
 
 const router = express.Router();
@@ -13,5 +14,5 @@ router.post('/food/:foodId',authUserMiddleware,postComment)
 router.get('/food/:foodId',authUserMiddleware,getComment)
 router.delete('/:commentId',authUserMiddleware,deleteComment)
 router.post('/reply/:commentId',authUserMiddleware,replyToComment)
-
+router.get('/replies/:commentId',authUserMiddleware,getReplies)
 module.exports = router;
